42aa58b34da856e84ac70a122ef70077c2bd91b8
MIsrarAhmad <m.israrahmad2021@gmail.com>
2024-06-14 20:50:32 -0700

    js assignment 01

---
+++ script.js
@@ -0,0 +1,27 @@
+let marks = +prompt("Enter your marks")
+if 
+    (marks >= 90 && marks <= 100) {
+        alert("you got grade A+")
+}
+else if (marks >= 80 && marks <= 89) {
+    alert("you got grade A")
+}
+else if(marks >= 70 && marks <= 79) {
+    alert("you got Grade B")
+}
+
+else if(marks >= 60 && marks <= 69) {
+    alert("you got Grade C")
+}
+else if(marks >= 50 && marks <= 59) {
+    alert("you got Grade D")
+}
+else if(marks <=50 && marks >=0 ) {
+    alert("you are fail janana bal zal try waka")
+}
+
+else{
+    alert("Invalid input")
+}
+
+
